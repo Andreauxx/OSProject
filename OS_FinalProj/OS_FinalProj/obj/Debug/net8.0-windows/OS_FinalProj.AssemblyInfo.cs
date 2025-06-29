e7f26681c3df2b8d507ae59bda3812917d6ee1d5
Jeon Andreaux B. Ampad <jaampad@mcm.edu.ph>
2025-06-29 21:27:35 +0800

    Vehicle Form Added

--- OS_FinalProj/OS_FinalProj/obj/Debug/net8.0-windows/OS_FinalProj.AssemblyInfo.cs
+++ OS_FinalProj/OS_FinalProj/obj/Debug/net8.0-windows/OS_FinalProj.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OS_FinalProj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70942ab339c79f36cb96350e08019ecf139079ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aff82c0c94e86fcbe3ac35f377b07416bfe261ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("OS_FinalProj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OS_FinalProj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
